ceda9393a762c8473730da0f6537119ad732499d
Warren Scipio <warrenscipio@gmail.com>
2023-03-10 17:16:20 -0800

    grab and throw things

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,10 +4,25 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+
+
 # Get the gravity from the project settings to be synced with RigidDynamicBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var neck := $Neck
 @onready var camera := $Neck/Camera3D
+@onready var interaction = $Neck/Camera3D/Interaction
+@onready var hand = $Neck/Camera3D/Hand
+@onready var joint = $Neck/Camera3D/Generic6DOFJoint3D
+@onready var staticBody = $Neck/Camera3D/StaticBody3D
+
+
+
+var picked_object
+var pull_power = 7
+var rotation_power =0.05
+var jump_count = 0
+var lockedCam = false
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
@@ -20,6 +35,17 @@ func _unhandled_input(event: InputEvent) -> void:
 			camera.rotate_x(-event.relative.y * 0.01)
 			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
 
+
+func pick_object():
+	var collider = interaction.get_collider()
+	if collider != null and collider is RigidBody3D:
+		picked_object = collider
+		joint.set_node_b(picked_object.get_path())
+		
+		
+func drop_object():
+	if picked_object != null:
+		picked_object = null
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -40,5 +66,18 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+		
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		if picked_object ==null:
+			pick_object()
+		elif picked_object != null:
+			drop_object()
+			
+			
+			
+	if picked_object != null:
+		var a = picked_object.global_transform.origin
+		var b = hand.global_transform.origin
+		picked_object.set_linear_velocity((b-a)*pull_power)
 
 	move_and_slide()
