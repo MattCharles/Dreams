aef1bf58a174cec9c33f527ec1edafe13588cec0
Matt Miller <matthew.c.anthony.miller@gmail.com>
2023-03-10 20:27:01 -0800

    Added finish line which emits a signal when colliding with a pizza

---
+++ Scripts/FinishLine.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+signal finished()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_area_3d_body_entered(body):
+	print("logging collision with " + str(body))
+	if body is Pizza:
+		print(" Pizza O_O ")
+		emit_signal("finished")
