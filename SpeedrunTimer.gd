7478a8b25d155a4841114fa7fdf3477f551d7d16
Matt Miller <matthew.c.anthony.miller@gmail.com>
2023-03-11 11:25:00 -0800

    Changed speedrun timer to display millis

--- SpeedrunTimer.gd
+++ SpeedrunTimer.gd
@@ -1,34 +1,42 @@
 extends Control
 
-@onready var timer = $Timer
 @onready var label = $Label
 
-var seconds := 0
-var minutes := 0
-var hours := 0
+var time = 0
+var timer_on := false
 
 func _ready():
+	timer_on = true
 	var finish_line = self.get_parent().get_node("FinishLine")
 	finish_line.finished.connect(stop_timer)
 
 func _process(delta):
-	pass
-
-
-func _on_timer_timeout():
-	update_display()
-
-func update_display():
-	seconds = seconds + 1
-	if seconds == 60:
-		seconds = 0
-		minutes = minutes + 1
-	if minutes == 60:
-		minutes = 0
-		hours = hours + 1
+	if not timer_on:
+		return
+	time += delta
 	
-	label.text = "%04d:%02d:%02d" % [hours, minutes, seconds]
+	var mills = fmod(time, 1) * 1000
+	var secs = fmod(time, 60)
+	var mins = fmod(time, 3600) / 60
+	var hours = fmod(fmod(time, 3600 * 60) / 3600, 24)
+	var days = fmod(time, 12960000) / 86400
+	var time_passed := ""
+	print(str(days))
+	if days >= 1:
+		time_passed = "%02d:%02d:%02d:%02d:%03d" % [days, hours, mins, secs, mills]
+	elif hours >= 1:
+		time_passed = "%02d:%02d:%02d:%03d" % [hours, mins, secs, mills]
+	else:
+		time_passed = "%02d:%02d:%03d" % [mins, secs, mills]
+	
+	label.text = time_passed
 
 func stop_timer():
-	print("stopping timer")
-	timer.stop()
+	timer_on = false
+
+func start_timer():
+	timer_on = true
+	
+func restart_timer():
+	timer_on = true
+	time = 0
