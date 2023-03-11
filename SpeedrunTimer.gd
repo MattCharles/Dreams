0d83ec6aca63e05dda19fbbe899ffb764c3c0417
Matt Miller <matthew.c.anthony.miller@gmail.com>
2023-03-11 00:48:42 -0800

    Added timer in top right of screen

---
+++ SpeedrunTimer.gd
@@ -0,0 +1,29 @@
+extends Control
+
+@onready var timer = $Timer
+@onready var label = $Label
+
+var seconds := 0
+var minutes := 0
+var hours := 0
+
+func _process(delta):
+	pass
+
+
+func _on_timer_timeout():
+	update_display()
+
+func update_display():
+	seconds = seconds + 1
+	if seconds == 60:
+		seconds = 0
+		minutes = minutes + 1
+	if minutes == 60:
+		minutes = 0
+		hours = hours + 1
+	
+	label.text = "%04d:%02d:%02d" % [hours, minutes, seconds]
+
+func stop_timer():
+	timer.stop()
